6b7a0ec6cc960108fef7bd66f2b0870e229799ce
pirgan <pirgan@gmail.com>
2021-04-13 22:22:23 +0200

    Completed module-5

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,21 @@
-import { addnewProduct, getProducts, getProductWithID } from '../controllers/controllers';
+import { addnewProduct, getProducts, getProductWithID, updateProduct, deleteProduct } from '../controllers/controllers';
 
 const routes = (app) => {
-    app.route('/products')
-        .get(getProducts)
-        .post(addnewProduct);
+app.route('/products')
+    .get(getProducts)
+    
+    // Post endpoint
+    .post(addnewProduct);
 
-    app.route('/products/:ProductID')
-        .get(getProductWithID)    
-}   
+app.route('/products/:ProductID')
+    // get a specific Product
+    .get(getProductWithID)
+
+    // updating a specific Product
+    .put(updateProduct)
+
+    // deleting a specific Product
+    .delete(deleteProduct);
+}
 
 export default routes;
